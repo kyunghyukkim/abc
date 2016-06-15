9841701e9bfc3a50d189ae9ddb78ed55122f4dd2
lapisfire <keagan@moomail.us>
2016-06-15 15:44:23 -0700

    Updated with perturbation and sigj

--- MooScripts.py
+++ MooScripts.py
@@ -1,5 +1,8 @@
 #!/Anaconda/python
 __author__ = 'Keagan Moo'
+import numpy
+
+
 def euclidd(list1, list2):
     iter = len(list1)
     sum = 0
@@ -19,3 +22,14 @@ def weightt(t, N, wgtp, pThet, pThetp, pTurb, piTurb):
         return piTurb(pThet)/sum
     else:
         return -666
+
+def Kpert(N, sigj, pThet, pThetp):
+    product = 1
+    for j in range(N):
+        bit = numpy.exp(-((pThet[j] - pThetp[j])**2)/((2*sigj)**2))/numpy.sqrt(2*3.14159*sigj)
+        product = product * bit
+    return product
+
+
+def sigmaj(var):
+    return var
