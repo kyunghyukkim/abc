a5291237a598d395ea60ab46027cc3c4e69f714d
lapisfire <keagan@moomail.us>
2016-06-16 22:18:18 -0700

    Added comments

--- MooScripts.py
+++ MooScripts.py
@@ -2,7 +2,9 @@
 __author__ = 'Keagan Moo'
 import numpy
 
-
+# euclidd (Euclidean Distance Function) will take two lists of points
+# and calculate the distance between the two using the euclidian distance
+# function. Which is described by sqrt((x1 - x2)^2+(y1 - y2)^2 ...)
 def euclidd(list1, list2):
     iter = len(list1)
     sum = 0
@@ -11,6 +13,11 @@ def euclidd(list1, list2):
         sum = sum + gap
     return sum ** (0.5)
 
+# weightt (Weight Calculator) takes a tolerance counter value,
+# a total number of particles, a previous weight vector,
+# a theta distribution, the previous theta distribution,
+# a perturbation function, and a proposal distribuiton to
+# create a new weighting vector for particles.
 def weightt(t, N, wgtp, pThet, pThetp, pTurb, piTurb):
     if t == 0:
         return 1
@@ -23,6 +30,10 @@ def weightt(t, N, wgtp, pThet, pThetp, pTurb, piTurb):
     else:
         return -666
 
+# Kpert (K perturbation function) stands in for the current perturbation kernel
+# it is defined online, but could be changed if needed. It takes the total number
+# of particles, a standard deviation and the current and previous parameter distributions
+# (theta).
 def Kpert(N, sigj, pThet, pThetp):
     product = 1
     for j in range(N):
@@ -30,6 +41,7 @@ def Kpert(N, sigj, pThet, pThetp):
         product = product * bit
     return product
 
-
+# sigmaj produces the standard deviation for the Kpert function from the various statistical
+# properties of the system.
 def sigmaj(var):
     return var
